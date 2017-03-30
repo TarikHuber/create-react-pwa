1dc7e3d13f309820ba5cbe0da16c7e8ff03210ec
TarikHuber <huber.tarik@gmail.com>
2017-03-30 21:01:36 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","38fbbf04f276699856771d721c0c0d2e"],["manifest.json","a30758a8c7b71bbc2ac9a632220ac955"],["static/css/main.9a0fe4f1.css","3473922d6aed4c20bb69846d6027cacf"],["static/js/main.a61377f0.js","8189ddf746e1d8728abad10bf83dd9f3"],["static/media/logo.5d5d9eef.svg","5d5d9eefa31e5e13a6610d9fa7a283bb"]];
+var precacheConfig = [["index.html","f08599c9be660552d41c154c03a69436"],["manifest.json","a30758a8c7b71bbc2ac9a632220ac955"],["static/css/main.9a0fe4f1.css","3473922d6aed4c20bb69846d6027cacf"],["static/js/main.8d6fd5c4.js","ce86616b02f2aef18ed925f064ef010c"],["static/media/logo.5d5d9eef.svg","5d5d9eefa31e5e13a6610d9fa7a283bb"]];
 var cacheName = 'sw-precache-v2-sw-precache-' + (self.registration ? self.registration.scope : '');
 
 
